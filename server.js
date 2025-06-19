a654f516ad89be12e1081db8746e077193146711
studentsharma <146518790+studentsharma@users.noreply.github.com>
2025-06-19 12:06:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,15 +29,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }));
 
-// Handle preflight requests
-app.options('*', cors({
-  origin: [
-    "https://chat-frontend-eypj.onrender.com",
-    "http://localhost:5173"
-  ], 
-  credentials: true
-}));
-
 
 
 app.use(express.json());
