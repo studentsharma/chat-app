f925c7f7c3979a8580a4dc7212d39d19e61fd69a
studentsharma <146518790+studentsharma@users.noreply.github.com>
2025-07-11 20:57:30 +0530

    just chaning server

--- server.js
+++ server.js
@@ -12,14 +12,22 @@ dotenv.config();
 
 const app = express();
 
+// app.use(cors({
+//   origin: [
+//     "https://chat-frontend-eypj.onrender.com",
+//     "http://localhost:5173"
+//   ], 
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
+// }));
+
 app.use(cors({
   origin: [
     "https://chat-frontend-eypj.onrender.com",
     "http://localhost:5173"
   ], 
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
 }));
 
 app.use(express.json());
